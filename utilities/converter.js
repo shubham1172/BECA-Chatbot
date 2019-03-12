1c76c8e6488d7f669228987ea7089b92fede4083
anujanegi <anujanegi1@gmail.com>
2019-03-12 17:12:10 +0530

    Updated numeric input handling

--- utilities/converter.js
+++ utilities/converter.js
@@ -1,6 +1,9 @@
 dict = {"first": 1, "second": 2, "third": 3, "fourth": 4}
 
 exports.ordinal_to_cardinal = (query_with_ordinal) => {
+  if (!isNaN(query_with_ordinal[0])) {
+    return Number(query_with_ordinal[0])
+  }
   words = query_with_ordinal.split(" ")
   for(var i=0; i<words.length; i++) {
     cardinal = dict[words[i]]
